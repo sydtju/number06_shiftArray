7c82dd07bd05095758e826d84aca3efd11d671fd
yadongshao <348829719@qq.com>
2015-03-28 23:30:05 +0800

    Create shiftarray.c

---
+++ shiftarray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+//绝对值的最大公约数
+int absgcd(int m,int n){
+	int temp=0;
+	m=m>0?m:-m;
+	n=n>0?n:-n;
+	while(m%n){
+		temp=n;
+		n=m%n;
+		m=temp;
+	}
+	return n;
+}
